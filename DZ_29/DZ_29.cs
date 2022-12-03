c9d074fff130de2f9dd98ce970346b8ccd64faf0
Артем Шустов <artemmeme2017@yandex.ru>
2022-12-03 16:30:26 +0300

    Написал программу для задания массива с клавиатуры и его вывода

--- DZ_29/DZ_29.cs
+++ DZ_29/DZ_29.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+int[] massive(int length)
+{
+    int[] massive = new int[length];
+    Console.WriteLine("Напишите числа массива через Enter");
+    for (int i = 0; i < length; i++)
+    {
+        massive[i] = Convert.ToInt32(Console.ReadLine()); ;
+    }
+    return massive;
+}
+int length = 8;
+int[] array = massive(length);
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
