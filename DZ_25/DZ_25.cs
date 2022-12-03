41cb90e3ae8def39cea3386a9c41370e1ccd8ea2
Артем Шустов <artemmeme2017@yandex.ru>
2022-12-03 14:04:32 +0300

    Написал программу для возведения A в степень B

--- DZ_25/DZ_25.cs
+++ DZ_25/DZ_25.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Напишите само число");
+int num0 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите степень возведения");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int num2 = 1;
+Vosvedenie(num2);
+void Vosvedenie(int num2)
+{
+    while (i < num1)
+    {
+        num2 = num2 * num0;
+        i++;
+    }
+    Console.WriteLine($"Число {num0} возвели в степень {num1}, получили {num2}");
+}
+
+
+
